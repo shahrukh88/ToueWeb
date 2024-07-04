d7f4badd6fd97f2a00d1fcd5e49944e935ab397f
shahrukh88 <66013821+shahrukh88@users.noreply.github.com>
2024-07-04 14:18:22 +0100

    Add files via upload

---
+++ ToueWebAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ToueWebAPI.Models;
+using ToueWebAPI.Models.DTO;
+
+namespace ToueWebAPI
+{
+    public class MappingConfig: Profile
+    {
+        public MappingConfig() {
+
+            CreateMap<Country, CountryDTO>();
+            CreateMap<CountryDTO, Country>();
+
+            CreateMap<Country, CountryCreateDTO>().ReverseMap();
+            CreateMap<Country, CountryUpdateDTO>().ReverseMap();
+        }
+    }
+
+}
--- ToueWebAPI/Program.cs
+++ ToueWebAPI/Program.cs
@@ -1,8 +1,22 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ToueWebAPI;
+using ToueWebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddControllers(option =>
+{
+
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
